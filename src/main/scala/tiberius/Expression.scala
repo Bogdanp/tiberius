f84a84f7cc2efed7309594bf367db34af7f1d385
Bogdan Popa <popa.bogdanp@gmail.com>
2013-08-26 01:18:17 +0300

    define basic expression hierarchy

---
+++ src/main/scala/tiberius/Expression.scala
@@ -0,0 +1,10 @@
+package tiberius
+
+sealed trait Expression
+case class NumberExp(n: Double) extends Expression
+case class StringExp(s: String) extends Expression
+case class SymbolExp(s: String) extends Expression
+case class StackExp(xs: Seq[Expression]) extends Expression
+case class FunctionExp(fn: Seq[Expression]) extends Expression
+case class NativeExp(fn: Env => Expression) extends Expression
+case class ApplicationExp(fn: Symbol) extends Expression
