8d960654600511dfa552ead4c4d1a3c89a92a2e1
Bogdan Popa <popa.bogdanp@gmail.com>
2013-08-26 01:41:15 +0300

    drop ApplicationExp and redefine FunctionExp in terms of a StackExp

--- src/main/scala/tiberius/Expression.scala
+++ src/main/scala/tiberius/Expression.scala
@@ -6,6 +6,5 @@ case class NumberExp(n: Double) extends Expression
 case class StringExp(s: String) extends Expression
 case class SymbolExp(s: String) extends Expression
 case class StackExp(xs: Seq[Expression]) extends Expression
-case class FunctionExp(fn: Seq[Expression]) extends Expression
-case class NativeExp(fn: Env => Expression) extends Expression
-case class ApplicationExp(fn: Symbol) extends Expression
+case class FunctionExp(s: SymbolExp, fn: StackExp) extends Expression
+case class NativeExp(s: SymbolExp, fn: Env => Expression) extends Expression
