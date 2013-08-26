756dbbcc44fc5d531301d47b77b608c9d1874b3e
Bogdan Popa <popa.bogdanp@gmail.com>
2013-08-26 03:35:52 +0300

    add REPL

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,45 @@
 import scala.io.Source
 
+import jline._
+
 import tiberius._
 import tiberius.std.Prelude
 
 object Main {
+  val prompt = "> "
+
   def main(args: Array[String]): Unit = {
     args match {
       case Array(filename) =>
         val source = Source.fromFile(filename).mkString
         val result = Tiberius.eval(filename, source, Prelude.env) match {
-          case Left(err) => s"error: ${err}"
+          case Left(err) => println(s"error: ${err}")
           case success   => success
         }
-      case _ => println("""Usage: sbt "run filename"""")
+      case Array() =>
+        Terminal.getTerminal.initializeTerminal
+
+        val reader = new ConsoleReader()
+
+        Iterator
+          .continually(reader.readLine(prompt))
+          .takeWhile(l => l != ":q" && l != null)
+          .foldLeft((Prelude.env, Tiberius.initialStack)) {
+            case ((env, stack), line) => {
+              Tiberius.eval("<stdin>", line, env, stack) match {
+                case Right((e, env, stack)) => {
+                  println(e.show)
+                  (env, stack)
+                }
+                case Left(err) => {
+                  println(s"error: ${err}")
+                  (env, stack)
+                }
+              }
+            }
+          }
+      case _ =>
+        println("""Usage: sbt "run [filename]"""")
     }
   }
 }
